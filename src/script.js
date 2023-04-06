eddeb776530bc22b595bc519aba0c42b787dd9fe
Niken Alfrido <alfrisiboro@gmail.com>
2023-04-07 04:17:55 +0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,38 @@
+let text = document.getElementById('text')
+let leaf = document.getElementById('leaf')
+let hill1 = document.getElementById('hill1')
+let hill4 = document.getElementById('hill4')
+let hill5 = document.getElementById('hill5')
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    if (value < 700) {
+        text.style.marginTop = value * 2.5 + 'px';
+        leaf.style.top = value * 1 + 'px';
+        leaf.style.left = value * 1.5 + 'px';
+        hill5.style.left = value * 1.5 + 'px';
+        hill4.style.left = value * -1.5 + 'px';
+        hill1.style.top = value * 1 + 'px';
+    }
+})
+
+var btnContainer = document.getElementById("menu");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("menu");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
+
